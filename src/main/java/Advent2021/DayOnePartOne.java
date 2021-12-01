66d08671173203a596466a33fcf0e740f9086e25
Paige Watson <paige.watson@outlook.com>
2021-12-01 09:41:53 -0800

    initial commit

---
+++ src/main/java/Advent2021/DayOnePartOne.java
@@ -0,0 +1,33 @@
+package Advent2021;
+
+import java.util.List;
+
+public class DayOnePartOne
+{
+    private final List<Integer> depthList;
+
+    public DayOnePartOne(List<Integer> depthList)
+    {
+
+        this.depthList = depthList;
+    }
+
+    public int increases()
+    {
+        int totalIncreases = 0;
+        if(depthList.size() ==1)
+        {
+            return 0;
+        }
+        int lastNumber = depthList.get(0);
+        for (int currentIndex = 1; currentIndex < depthList.size(); currentIndex++)
+        {
+            final Integer currentDepth = depthList.get(currentIndex);
+            if(currentDepth >lastNumber){
+                totalIncreases++;
+            }
+            lastNumber = currentDepth;
+        }
+        return totalIncreases;
+    }
+}
---
+++ src/main/java/Advent2021/DayOnePartTwo.java
@@ -0,0 +1,67 @@
+package Advent2021;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class DayOnePartTwo
+{
+
+    public DayOnePartTwo()
+    {
+    }
+
+    public int sum(List<Integer> integerList)
+    {
+        int totalSum = integerList.stream().mapToInt(integer -> integer).sum();
+        return totalSum;
+    }
+//
+//    public Integer[] splitToArrays(List<Integer> integerList)
+//    {
+//        final int numberOfSets = integerList.size() % 3;
+//        for (int currentIndex = 0; currentIndex < numberOfSets; currentIndex += 3)
+//        {
+//            integerList.get(currentIndex);
+//        }
+//        return new Integer[0];
+//    }
+
+
+    public List<List<Integer>> splitIntoSets(List<Integer> integerList)
+    {
+        List<List<Integer>> sets = new ArrayList<>();
+        int numberOfSets = integerList.size() - 2;
+        for (int setIndex = 0; setIndex < numberOfSets; setIndex++)
+        {
+            final List<Integer> integers = integerList.subList(setIndex, setIndex + 3);
+            sets.add(integers);
+        }
+        return sets;
+    }
+
+    public int countHigherSets(List<List<Integer>> setList)
+    {
+        int lastSum = 0;
+        int totalHigherSets = 0;
+        for (List<Integer> integers : setList)
+        {
+            final int setSum = sum(integers);
+            if (lastSum != 0 && setSum > lastSum)
+            {
+                totalHigherSets++;
+            }
+            lastSum = setSum;
+
+        }
+        return totalHigherSets;
+    }
+
+    public int processList(List<Integer> integerList)
+    {
+        final List<List<Integer>> intoSets = splitIntoSets(integerList);
+        final int higherSets = countHigherSets(intoSets);
+        return higherSets;
+    }
+}
